cc0abfa98085ff8d77a3424374c156b19f8b7149
Talen Fisher <tqfisher@iastate.edu>
2018-11-13 12:44:38 -0600

    fix tabbing in queue assignments

--- assignments/queues/Queue.java
+++ assignments/queues/Queue.java
@@ -9,118 +9,118 @@ public class Queue {
      * Psuedo circular array for the queue
      */
     private String[] queue;
-    
+
     /**
      * Front pointer
      * This can never be -1
      */
     private int front = 0; // front can never be -1
-    
+
     /**
      * Rear pointer
      * No elements added so far, has to be -1 initially
      */
-	private int rear = -1; 
-	
-	/**
-	 * Constructor, creates a new queue of a specified size
-	 * @param size the maximum size of the queue
-	 */
-	public Queue(int size) {
-	    queue = new String[size];
-	}
-	
-	/**
-	 * Helper method for determining real pointers. The pointers
-	 * keep incrementing beyond the array size so this method gets
-	 * the real position of the pointer in the array.
-	 * 
-	 * @param pointer the pointer to reference
-	 * @return the real position in the array
-	 */
-	private int getRealPointer(int pointer) {
-		// modulus calculation is only performed if pointer is at or beyond array size
-		return (pointer >= queue.length) ? pointer % queue.length : pointer;
-	}
-	
-	/**
-	 * Add an item to the end of the queue if the queue is
-	 * not full.
-	 * 
-	 * @param item the item to add
-	 * @return the item added if successful and null if not
-	 */
-	public String enqueue(String item) {
-		return (!isFull()) ? queue[getRealPointer(++rear)] = item : null;
-	}
-	
-	/**
-	 * Remove an item from the beginning of the queue.
-	 * 
-	 * @return the item at the beginning of the queue or null if empty
-	 */
-	public String dequeue() {
-		// there is no point in nulling the element since it will get reassigned later
-		return (!isEmpty()) ? queue[getRealPointer(front++)] : null;
-	}
-	
-	/**
-	 * Return the item at the beginning of the queue
-	 * but does not modify the queue
-	 * 
-	 * @return the item at the beginning of the queue or null if empty
-	 */
-	public String peek() {
-		return (!isEmpty()) ? queue[getRealPointer(front)] : null;
-	}
-	
-	/**
-	 * Check to see if the queue is full
-	 * 
-	 * @return true if full and false if empty
-	 */
-	public boolean isFull() {
-		return (size() == queue.length);
-	}
-	
-	
-	/**
-	 * Check to see if the queue is empty
-	 * 
-	 * @return true if empty and false if not
-	 */
-	public boolean isEmpty() {
-		return (size() == 0);
-	}
-	
-	/**
-	 * Get the size of the queue
-	 * 
-	 * @return the current amount of elements in the queue
-	 */
-	public int size() {
-		return (rear - front) + 1; // add 1 because the storage is zero indexed
-	}
-	
-	/**
-	 * Print the queue
-	 */
-	public void print() {
-		System.out.print(this);
-	}
-	
-	/**
-	 * Convert the queue to a string
-	 */
-	public String toString() {
-		String out = "[";
-		
-		for(int i = front; index <= rear; i++) {
-			out += queue[getRealPointer(i)];
-			if(i != rear) out += ", "; // only add a comma if not on the last element 
+    private int rear = -1; 
+
+    /**
+     * Constructor, creates a new queue of a specified size
+     * @param size the maximum size of the queue
+     */
+    public Queue(int size) {
+        queue = new String[size];
+    }
+
+    /**
+     * Helper method for determining real pointers. The pointers
+     * keep incrementing beyond the array size so this method gets
+     * the real position of the pointer in the array.
+     * 
+     * @param pointer the pointer to reference
+     * @return the real position in the array
+     */
+    private int getRealPointer(int pointer) {
+        // modulus calculation is only performed if pointer is at or beyond array size
+        return (pointer >= queue.length) ? pointer % queue.length : pointer;
+    }
+
+    /**
+     * Add an item to the end of the queue if the queue is
+     * not full.
+     * 
+     * @param item the item to add
+     * @return the item added if successful and null if not
+     */
+    public String enqueue(String item) {
+        return (!isFull()) ? queue[getRealPointer(++rear)] = item : null;
+    }
+
+    /**
+     * Remove an item from the beginning of the queue.
+     * 
+     * @return the item at the beginning of the queue or null if empty
+     */
+    public String dequeue() {
+        // there is no point in nulling the element since it will get reassigned later
+        return (!isEmpty()) ? queue[getRealPointer(front++)] : null;
+    }
+
+    /**
+     * Return the item at the beginning of the queue
+     * but does not modify the queue
+     * 
+     * @return the item at the beginning of the queue or null if empty
+     */
+    public String peek() {
+        return (!isEmpty()) ? queue[getRealPointer(front)] : null;
+    }
+
+    /**
+     * Check to see if the queue is full
+     * 
+     * @return true if full and false if empty
+     */
+    public boolean isFull() {
+        return size() == queue.length;
+    }
+
+
+    /**
+     * Check to see if the queue is empty
+     * 
+     * @return true if empty and false if not
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * Get the size of the queue
+     * 
+     * @return the current amount of elements in the queue
+     */
+    public int size() {
+        return (rear - front) + 1; // add 1 because the storage is zero indexed
+    }
+
+    /**
+     * Print the queue
+     */
+    public void print() {
+        System.out.print(this);
+    }
+
+    /**
+     * Convert the queue to a string
+     */
+    public String toString() {
+        String out = "[";
+
+        for(int i = front; index <= rear; i++) {
+            out += queue[getRealPointer(i)];
+            if(i != rear) out += ", "; // only add a comma if not on the last element 
         }
-        
-		out += "]";
-		return out;
-	}
+
+        out += "]";
+        return out;
+    }
 }
--- assignments/queues/QueueDriver.java
+++ assignments/queues/QueueDriver.java
@@ -5,58 +5,58 @@
  * @author Talen Fisher
  */
 public class QueueDriver {
-	
-	/**
-	 * Program entry point
+
+    /**
+     * Program entry point
      * 
-	 * @param args command line args
-	 */
-	public static void main(String[] args) {
-		// create the queue
-		Queue queue = new Queue(5);
-		
-		// run some tests
-		printQueueAction("enqueue(dog)", queue.enqueue("dog"), queue);
-		printQueueAction("enqueue(cat)", queue.enqueue("cat"), queue);
-		printQueueAction("enqueue(mouse)", queue.enqueue("mouse"), queue);
-		printQueueAction("enqueue(pig)", queue.enqueue("pig"), queue);
-		printQueueAction("enqueue(bird)", queue.enqueue("bird"), queue);
-		printQueueAction("size()", queue.size(), queue);
-		printQueueAction("enqueue(puppy)", queue.enqueue("puppy"), queue);
-		printQueueAction("size()", queue.size(), queue);
-		printQueueAction("dequeue()", queue.dequeue(), queue);
-		printQueueAction("dequeue()", queue.dequeue(), queue);
-		printQueueAction("size()", queue.size(), queue);
-		printQueueAction("dequeue()", queue.dequeue(), queue);
-		printQueueAction("peek()", queue.peek(), queue);
-		printQueueAction("dequeue()", queue.dequeue(), queue);
-		printQueueAction("dequeue()", queue.dequeue(), queue);
-		printQueueAction("size()", queue.size(), queue);
-		printQueueAction("isEmpty()", queue.isEmpty(), queue);
-		
-		// add more elements to demonstrate use of the circular array
-		printQueueAction("enqueue(rabbit)", queue.enqueue("rabbit"), queue);
-		printQueueAction("enqueue(turtle)", queue.enqueue("turtle"), queue);
-		printQueueAction("enqueue(fish)", queue.enqueue("fish"), queue);
-		printQueueAction("enqueue(shark)", queue.enqueue("shark"), queue);
-		printQueueAction("enqueue(spider)", queue.enqueue("spider"), queue);
-		printQueueAction("enqueue(groundhog)", queue.enqueue("groundhog"), queue);
-		printQueueAction("isFull()", queue.isFull(), queue);
-		
-	}
-	
-	/**
-	 * Function for printing out queue tests/actions in a uniform format.
-	 * Example output:
-	 * enqueue(test):                        test [test]
-	 *   
-	 * @param key the test key/name (string representation of the method call)
-	 * @param value the value returned from the method that was called (this can be anything)
-	 * @param queue the queue used for the test
-	 */
-	public static <Value> void printQueueAction(String call, Value value, Queue queue) {
-		System.out.printf("%-20s %20s ", call+":", value);
-		queue.print(); // output stack contents
-		System.out.println();
-	}
+     * @param args command line args
+     */
+    public static void main(String[] args) {
+        // create the queue
+        Queue queue = new Queue(5);
+
+        // run some tests
+        printQueueAction("enqueue(dog)", queue.enqueue("dog"), queue);
+        printQueueAction("enqueue(cat)", queue.enqueue("cat"), queue);
+        printQueueAction("enqueue(mouse)", queue.enqueue("mouse"), queue);
+        printQueueAction("enqueue(pig)", queue.enqueue("pig"), queue);
+        printQueueAction("enqueue(bird)", queue.enqueue("bird"), queue);
+        printQueueAction("size()", queue.size(), queue);
+        printQueueAction("enqueue(puppy)", queue.enqueue("puppy"), queue);
+        printQueueAction("size()", queue.size(), queue);
+        printQueueAction("dequeue()", queue.dequeue(), queue);
+        printQueueAction("dequeue()", queue.dequeue(), queue);
+        printQueueAction("size()", queue.size(), queue);
+        printQueueAction("dequeue()", queue.dequeue(), queue);
+        printQueueAction("peek()", queue.peek(), queue);
+        printQueueAction("dequeue()", queue.dequeue(), queue);
+        printQueueAction("dequeue()", queue.dequeue(), queue);
+        printQueueAction("size()", queue.size(), queue);
+        printQueueAction("isEmpty()", queue.isEmpty(), queue);
+
+        // add more elements to demonstrate use of the circular array
+        printQueueAction("enqueue(rabbit)", queue.enqueue("rabbit"), queue);
+        printQueueAction("enqueue(turtle)", queue.enqueue("turtle"), queue);
+        printQueueAction("enqueue(fish)", queue.enqueue("fish"), queue);
+        printQueueAction("enqueue(shark)", queue.enqueue("shark"), queue);
+        printQueueAction("enqueue(spider)", queue.enqueue("spider"), queue);
+        printQueueAction("enqueue(groundhog)", queue.enqueue("groundhog"), queue);
+        printQueueAction("isFull()", queue.isFull(), queue);
+
+    }
+
+    /**
+     * Function for printing out queue tests/actions in a uniform format.
+     * Example output:
+     * enqueue(test):                        test [test]
+     *   
+     * @param key the test key/name (string representation of the method call)
+     * @param value the value returned from the method that was called (this can be anything)
+     * @param queue the queue used for the test
+     */
+    public static <Value> void printQueueAction(String call, Value value, Queue queue) {
+        System.out.printf("%-20s %20s ", call+":", value);
+        queue.print(); // output stack contents
+        System.out.println();
+    }
 }
