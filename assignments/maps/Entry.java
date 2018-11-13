b47ba8ca4423c95a867cbabe2028c0040e282368
Talen Fisher <tqfisher@iastate.edu>
2018-11-13 12:52:07 -0600

    fix tabbing in map assignment

--- assignments/maps/Entry.java
+++ assignments/maps/Entry.java
@@ -6,86 +6,86 @@
  * @param <Value> the value type
  */
 public class Entry<Key,Value> {
-    
+
     /**
      * The entry's key
      */
     private Key key;
-    
+
     /**
      * The entry's value
      */
     private Value value;
-    
+
     /**
      * Next entry in the map
      */
-	private Entry<Key,Value> next;
-	
-	/**
-	 * Constructor
+    private Entry<Key,Value> next;
+
+    /**
+     * Constructor
      * 
-	 * @param key the key for the entry
-	 * @param value the value for the entry
-	 */
-	public Entry(Key key, Value value) {
-		this.key = key;
-		this.value = value;
-	}
-	
-	
-	/**
-	 * getter for the key property
-	 * key is readonly after set by the constructor
+     * @param key the key for the entry
+     * @param value the value for the entry
+     */
+    public Entry(Key key, Value value) {
+        this.key = key;
+        this.value = value;
+    }
+
+
+    /**
+     * getter for the key property
+     * key is readonly after set by the constructor
      * 
-	 * @return the key
-	 */
-	public Key getKey() {
-		return key;
-	}
-	
-	/**
-	 * getter for the value property
+     * @return the key
+     */
+    public Key getKey() {
+        return key;
+    }
+
+    /**
+     * getter for the value property
      * 
-	 * @return the value
-	 */
-	public Value getValue() {
-		return value;
-	}
-	
-	/**
-	 * setter for the value property
+     * @return the value
+     */
+    public Value getValue() {
+        return value;
+    }
+
+    /**
+     * setter for the value property
      * 
-	 * @param value the value to set
-	 * @return the value set
-	 */
-	public Value setValue(Value value) {
-		return this.value = value;
-	}
-	
-	/**
-	 * Convert the entry to a string
-	 */
-	public String toString() {
-		return key + ": " + value;
-	}
-	
-	/**
-	 * getter for the next entry
+     * @param value the value to set
+     * @return the value set
+     */
+    public Value setValue(Value value) {
+        return this.value = value;
+    }
+
+    /**
+     * Convert the entry to a string
+     */
+    public String toString() {
+        return key + ": " + value;
+    }
+
+    /**
+     * getter for the next entry
      * 
-	 * @return the next entry
-	 */
-	public Entry<Key, Value> getNext() {
-		return next;
-	}
-	
-	/**
-	 * setter for the next entry property
+     * @return the next entry
+     */
+    public Entry<Key, Value> getNext() {
+        return next;
+    }
+
+    /**
+     * setter for the next entry property
      * 
-	 * @param next the next entry
-	 * @return the next entry that was set
-	 */
-	public Entry<Key,Value> setNext(Entry<Key,Value> next) {
-		return this.next = next;
-	}
+     * @param next the next entry
+     * @return the next entry that was set
+     */
+    public Entry<Key,Value> setNext(Entry<Key,Value> next) {
+        return this.next = next;
+    }
 }
--- assignments/maps/MapDriver.java
+++ assignments/maps/MapDriver.java
@@ -3,58 +3,58 @@
  * @author Talen Fisher
  */
 public class MapDriver {
-	public static void main(String args[]) {
-		// create a map
-		Map<String,String> map = new Map<>();
-		
-		// insert some key value pairs and display
-		map.set("sarah@gmail.com", "Sarah");
-		map.set("ben123@yahoo.com", "Ben");
-		map.set("vdubz@msn.com", "Victoria");
+    public static void main(String args[]) {
+        // create a map
+        Map<String,String> map = new Map<>();
+
+        // insert some key value pairs and display
+        map.set("sarah@gmail.com", "Sarah");
+        map.set("ben123@yahoo.com", "Ben");
+        map.set("vdubz@msn.com", "Victoria");
         map.set("msn@aol.com", "Bill");
-        
-		System.out.println("Inserted 4 key:value pairs");
-        
+
+        System.out.println("Inserted 4 key:value pairs");
+
         map.display();
-		System.out.println("\n");
-		
-		// test delete method, check size before and after
-		System.out.println("size(): " + map.size());
-		System.out.println("delete(vdubz@msn.com): " + map.delete("vdubz@msn.com"));
+        System.out.println("\n");
+
+        // test delete method, check size before and after
         System.out.println("size(): " + map.size());
-        
-		map.display();
-		System.out.println("\n");
-		
-		// set another entry, check size, if empty, find a couple keys and get the value of a key
-		System.out.println("set(apple@hotmail.com, Megan): " + map.set("apple@hotmail.com", "Megan"));
-		System.out.println("isEmpty(): " + map.isEmpty());
-		System.out.println("size(): " + map.size());
-		System.out.println("findKey(vdubz@msn.com): " + map.findKey("vdubz@msn.com"));
-		System.out.println("findKey(apple@hotmail.com): " + map.findKey("apple@hotmail.com"));
-		System.out.println("getValue(ben123@yahoo.com): " + map.getValue("ben123@yahoo.com"));
-        
+        System.out.println("delete(vdubz@msn.com): " + map.delete("vdubz@msn.com"));
+        System.out.println("size(): " + map.size());
+
         map.display();
-		System.out.println("\n");
-		
-		// iterate through each entry and print it
-		System.out.println("Iterating through each entry and printing them:");
-        
+        System.out.println("\n");
+
+        // set another entry, check size, if empty, find a couple keys and get the value of a key
+        System.out.println("set(apple@hotmail.com, Megan): " + map.set("apple@hotmail.com", "Megan"));
+        System.out.println("isEmpty(): " + map.isEmpty());
+        System.out.println("size(): " + map.size());
+        System.out.println("findKey(vdubz@msn.com): " + map.findKey("vdubz@msn.com"));
+        System.out.println("findKey(apple@hotmail.com): " + map.findKey("apple@hotmail.com"));
+        System.out.println("getValue(ben123@yahoo.com): " + map.getValue("ben123@yahoo.com"));
+
+        map.display();
+        System.out.println("\n");
+
+        // iterate through each entry and print it
+        System.out.println("Iterating through each entry and printing them:");
+
         for(Entry<String,String> entry : map) {
-			System.out.println(entry);
+            System.out.println(entry);
         }
-        
-		System.out.println("\n");
-		
-		// delete all entries, check size and if empty
-		System.out.println("Deleting all entries...");
-        
+
+        System.out.println("\n");
+
+        // delete all entries, check size and if empty
+        System.out.println("Deleting all entries...");
+
         for(Entry<String,String> entry : map) {
-			map.delete(entry.getKey());
+            map.delete(entry.getKey());
         }
-        
-		System.out.println(map);
-		System.out.println("size(): " + map.size());
-		System.out.println("isEmpty(): " + map.isEmpty());
-	}
+
+        System.out.println(map);
+        System.out.println("size(): " + map.size());
+        System.out.println("isEmpty(): " + map.isEmpty());
+    }
 }
